55f9dc62de0a7a01b6bec92346c985f6c432dffe
Liamcodeshard <99624436+Liamcodeshard@users.noreply.github.com>
2023-03-20 18:44:01 +0000

    Initial commit

---
+++ Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEditor;
+using UnityEngine;
+using EditorGUITable;
+
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+
+
+public class CustomTerrainEditor : Editor
+{
+    SerializedProperty randomHeightRange;
+
+    // allows this secttion ot be dropped down
+    bool showRandom = false;
+
+    void OnEnable()
+    {
+        //matches the property with the one on the terrain
+        randomHeightRange = serializedObject.FindProperty("randomHeightRange");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+
+        // editor scripts recognise the target as the type (above)
+        CustomTerrain terrain = (CustomTerrain)target;
+
+        // this is the logic purely behind the arrow
+        showRandom = EditorGUILayout.Foldout(showRandom, "Random");
+
+        // this is what we see when the arrow is down
+        if(showRandom)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("Set Heights Between Random Values", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(randomHeightRange);
+            if(GUILayout.Button("Random Heights"))
+            {
+                terrain.RandomTerrain();
+            }
+        }
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour
+{
+    public Vector2 randomHeightRange = new Vector2(0,0.1f);
+
+    public void RandomTerrain()
+    {
+
+    }
+
+
+    void Awake()
+    {
+        // gets the unity object that manages tags
+        SerializedObject tagManager = new SerializedObject(
+            AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+        // acess the tag property
+        SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+
+        // using the tag property we create tags
+        AddTag(tagsProp, "Terrain");
+        AddTag(tagsProp, "Cloud");
+        AddTag(tagsProp, "Shore");
+
+        //apply tag changes to tag datatbase
+        tagManager.ApplyModifiedProperties();
+
+        // tag this gameobject
+        this.gameObject.tag = "Terrain";
+    }
+
+    void AddTag(SerializedProperty tagsProp, string newTag)
+    {
+        bool found = false;
+
+        //ensure tag doesnt exist
+        for(int i = 0; i < tagsProp.arraySize; i++)
+        {
+            SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+            if (t.stringValue.Equals(newTag)) { found = true; break; }
+        }
+
+        if (!found)
+        {
+            tagsProp.InsertArrayElementAtIndex(0);
+            SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+            newTagProp.stringValue = newTag;
+        }
+    }
+
+
+}
