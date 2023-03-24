b9357cfe03a55658a037791ba9511707ca01bc5f
Liamcodeshard <99624436+Liamcodeshard@users.noreply.github.com>
2023-03-24 18:44:37 +0000

    changed bare shit brooo

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+    public static float fBM(float x, float y, int oct, float persistance, int offestX, int offsetY)
+    {
+        float total = 0f;
+        float frequency = 1;
+        float amplitude = 1;
+        float maxValue = 0;
+
+        for (int i = 0; i < oct; i++)
+        {
+            total += Mathf.PerlinNoise((x + offestX) * frequency, (y + offsetY) * frequency) * amplitude;
+            maxValue += amplitude;
+            amplitude *= persistance;
+            frequency = 2;
+        }
+        return total / maxValue;
+    }
+
+}
